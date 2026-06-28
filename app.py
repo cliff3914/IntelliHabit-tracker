0bee31d369cdc901b4351a80dfe2e41ac57735a6
cliff3914 <bwanalicliff703@gmail.com>
2026-06-28 05:10:12 +0200

    Switch to Outlook SMTP

--- app.py
+++ app.py
@@ -59,13 +59,11 @@ VAPID_PRIVATE_KEY = os.environ.get('VAPID_PRIVATE_KEY')
 VAPID_PUBLIC_KEY = os.environ.get('VAPID_PUBLIC_KEY')
 VAPID_EMAIL = os.environ.get('VAPID_EMAIL', 'mailto:your-email@gmail.com')
 
-# Mail configuration
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_SERVER'] = 'smtp-mail.outlook.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME')
 
 mail = Mail(app)
 login_manager = LoginManager()
